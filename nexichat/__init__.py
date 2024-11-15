80f8599a4ddc5c28df9382aa32f9789edca06575
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-11-15 14:27:55 +0530

    Update __init__.py

--- nexichat/__init__.py
+++ nexichat/__init__.py
@@ -5,7 +5,7 @@ import uvloop
 from Abg import patch
 from pyrogram import Client
 from motor.motor_asyncio import AsyncIOMotorClient
-
+from pyrogram.types import BotCommand
 import config
 
 from .misc import LOGGER
@@ -39,6 +39,27 @@ class nexichat(Client):
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
         self.mention = self.me.mention
+        try:
+            await self.set_bot_commands(
+                commands=[
+                    BotCommand("start", "Start the bot"),
+                    BotCommand("help", "Get the help menu"),
+                    BotCommand("clone", "Make your own chatbot"),
+                    BotCommand("ping", "Check if the bot is alive or dead"),
+                    BotCommand("lang", "Select bot reply language"),
+                    BotCommand("resetlang", "Reset to default bot reply lang"),
+                BotCommand("id", "Get users user_id"),
+                BotCommand("stats", "Check bot stats"),
+                BotCommand("gcast", "Broadcast any message to groups/users"),
+                BotCommand("chatbot", "Enable or disable chatbot"),
+                BotCommand("status", "Check chatbot enable or disable in chat"),
+                BotCommand("shayri", "Get random shayri for love"),
+                BotCommand("repo", "Get chatbot source code"),
+                ]
+            )
+            LOGGER.info("Bot commands set successfully.")
+        except Exception as ex:
+            LOGGER.error(f"Failed to set bot commands: {ex}")
 
 
 nexichat = nexichat()
