93b4f85ba6a7ab17d1ad6702ef41cfe2f581883c
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-11-15 14:13:28 +0530

    Update __init__.py

--- nexichat/__init__.py
+++ nexichat/__init__.py
@@ -4,24 +4,13 @@ import time
 from Abg import patch
 from motor.motor_asyncio import AsyncIOMotorClient
 from pyrogram import Client
+from .misc import LOGGER
 
 import config
 import uvloop
 import time
 CLONE_OWNERS = {}
 uvloop.install()
-
-logging.basicConfig(
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
-
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-
-
-LOGGER = logging.getLogger(__name__)
 boot = time.time()
 mongodb = AsyncIOMotorClient(config.MONGO_URL)
 db = mongodb.Anonymous
