5f8cac065eca761bd2c44dca41f41ebfe6595162
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-11-15 14:13:54 +0530

    Update misc.py

--- nexichat/misc.py
+++ nexichat/misc.py
@@ -1,8 +1,20 @@
 import time
+import logging
 import asyncio
 
 from . import boot
 
+logging.basicConfig(
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+
+LOGGER = logging.getLogger(__name__)
 
 async def get_uptime():
     def _get_uptime():
